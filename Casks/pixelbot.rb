47d0a1d338872ee2c6f250f3ddb7a43c30668a0a
Dony Pramujo <donypramujo@gmail.com>
2026-08-30 17:02:50 +0700

    feat: release pixelbot v1.13.0

--- Casks/pixelbot.rb
+++ Casks/pixelbot.rb
@@ -1,6 +1,6 @@
 cask "pixelbot" do
   version "1.13.0"
-  sha256 "6b2e06ae4a89572402b899181217173d2dd845e2afb66883412011f6148db9a6"
+  sha256 "c658b339e1716e34d8321ad580cd5af01b7f16de02e5cb75e00da16a6bd26ba1"
 
   url "https://github.com/pixel-labs-id/homebrew-tap/releases/download/v1.13.0/PixelBot-1.13.0-macOS-arm64.dmg"
   name "PixelBot"
